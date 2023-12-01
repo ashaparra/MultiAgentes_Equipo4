7d5804274ef652080535c8d6c6d315f3748877e2
Arantza Parra <A01782023@tec.mx>
2023-12-01 11:39:48 -0600

    adjusted AgentController

--- AgentsVisualization/TrafficVisualization/Assets/Scripts/AgentController.cs
+++ AgentsVisualization/TrafficVisualization/Assets/Scripts/AgentController.cs
@@ -157,93 +157,99 @@ public class AgentController : MonoBehaviour
     private float timer, dt;
 
     void Start()
-{
-    // Initialize the data
-    agentsData = new AgentsData();
-    obstacleData = new AgentsData();
-    trafficLightsData = new TrafficLightsData();
-    roadsData = new AgentsData();
-    destinationsData = new AgentsData();
-
-    prevPositions = new Dictionary<string, Vector3>();
-    currPositions = new Dictionary<string, Vector3>();
-
-    agents = new Dictionary<string, GameObject>();
-    trafficLights = new Dictionary<string, GameObject>();
-    
-    timer = timeToUpdate;
-
-    // Start a coroutine to send the configuration to the server.
-    StartCoroutine(SendConfiguration());
-}
-
-private void Update() 
-{
-    // If the timer has reached zero
-    if(timer < 0)
     {
-        // Reset the timer and start the update process
+        // Initialize the data
+        agentsData = new AgentsData();
+        obstacleData = new AgentsData();
+        trafficLightsData = new TrafficLightsData();
+        roadsData = new AgentsData();
+        destinationsData = new AgentsData();
+
+        prevPositions = new Dictionary<string, Vector3>();
+        currPositions = new Dictionary<string, Vector3>();
+
+        agents = new Dictionary<string, GameObject>();
+        trafficLights = new Dictionary<string, GameObject>();
+        
         timer = timeToUpdate;
-        updated = false;
-        StartCoroutine(UpdateSimulation());
+
+        // Start a coroutine to send the configuration to the server.
+        StartCoroutine(SendConfiguration());
     }
 
-    // If the simulation has been updated
-    if (updated)
+    private void Update() 
     {
-        // Decrease the timer by the time since the last frame
-        timer -= Time.deltaTime;
-        dt = 1.0f - (timer / timeToUpdate);
-        // Update the state of each traffic light
-        foreach(var tl in trafficLightsData.positions)
+        // If the timer has reached zero
+        if(timer < 0)
         {
-            if(trafficLights.ContainsKey(tl.id))
+            // Reset the timer and start the update process
+            timer = timeToUpdate;
+            updated = false;
+            StartCoroutine(UpdateSimulation());
+        }
+
+        // If the simulation has been updated
+        if (updated)
+        {
+            // Decrease the timer by the time since the last frame
+            timer -= Time.deltaTime;
+            dt = 1.0f - (timer / timeToUpdate);
+            // Update the state of each traffic light
+            foreach(var tl in trafficLightsData.positions)
             {
-                trafficLights[tl.id].GetComponent<TrafficLight>().SetState(tl.state);
+                if(trafficLights.ContainsKey(tl.id))
+                {
+                    trafficLights[tl.id].GetComponent<TrafficLight>().SetState(tl.state);
+                }
             }
         }
     }
-}
 
-IEnumerator UpdateSimulation()
-{
-    // Send a GET request to the server to update the simulation
-    UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
-    yield return www.SendWebRequest();
- 
-    // If the request failed, log the error
-    if (www.result != UnityWebRequest.Result.Success)
-        Debug.Log(www.error);
-    else 
+    IEnumerator UpdateSimulation()
     {
-        // If the request succeeded, start coroutines to get the data of the agents and traffic lights
-        StartCoroutine(GetAgentsData());
-        StartCoroutine(GetTrafficLightsData());
+        // Send a GET request to the server to update the simulation
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + updateEndpoint);
+        yield return www.SendWebRequest();
+    
+        // If the request failed, log the error
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.Log(www.error);
+        else 
+        {
+            // If the request succeeded, start coroutines to get the data of the agents and traffic lights
+            StartCoroutine(GetAgentsData());
+            StartCoroutine(GetTrafficLightsData());
+        }
     }
-}
 
-IEnumerator SendConfiguration()
-{
-    // Create a form with the number of agents, width, and height
-    WWWForm form = new WWWForm();
-    form.AddField("NAgents", NAgents.ToString());
-    form.AddField("width", width.ToString());
-    form.AddField("height", height.ToString());
-
-    // Send a POST request to the server with the form
-    UnityWebRequest www = UnityWebRequest.Post(serverUrl + sendConfigEndpoint, form);
-    www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    yield return www.SendWebRequest();
-
-    // If the request failed, log the error
-    if (www.result != UnityWebRequest.Result.Success)
+    IEnumerator SendConfiguration()
     {
-        Debug.Log(www.error);
-    }
-    else
-    {
-       
+        /*
+        The SendConfiguration method is used to send the configuration to the server.
+
+        It uses a WWWForm to send the data to the server, and then it uses a UnityWebRequest to send the form.
+        */
+        WWWForm form = new WWWForm();
+
+        form.AddField("NAgents", NAgents.ToString());
+        form.AddField("width", width.ToString());
+        form.AddField("height", height.ToString());
+
+        // Send a POST request to the server with the form
+        UnityWebRequest www = UnityWebRequest.Post(serverUrl + sendConfigEndpoint, form);
+        www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        yield return www.SendWebRequest();
+
+        // If the request failed, log the error
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Configuration upload complete!");
+            Debug.Log("Getting Agents positions");
             // Once the configuration has been sent, it launches a coroutine to get the agents data.
             StartCoroutine(GetAgentsData());
             StartCoroutine(GetObstacleData());
@@ -252,12 +258,12 @@ IEnumerator SendConfiguration()
             StartCoroutine(GetDestinationsData());
         }
     }
-   IEnumerator GetAgentsData() 
+    IEnumerator GetAgentsData() 
 {
-    // Send a GET request to the server to get the data of the agents
+        // Send a GET request to the server to get the data of the agents
     UnityWebRequest www = UnityWebRequest.Get(serverUrl + getAgentsEndpoint);
     yield return www.SendWebRequest();
- 
+    
     if (www.result != UnityWebRequest.Result.Success)
     {
         // If the request failed, log the error
@@ -270,7 +276,7 @@ IEnumerator SendConfiguration()
         HashSet<string> receivedIds = new HashSet<string>();
         foreach(AgentData agent in agentsData.positions)
         {
-            // Add the id of each agent to the set of received ids
+             // Add the id of each agent to the set of received ids
             receivedIds.Add(agent.id);
             Vector3 newAgentPosition = new Vector3(agent.x, agent.y, agent.z);
 
@@ -282,7 +288,7 @@ IEnumerator SendConfiguration()
             }
             else
             {
-                // If the agent does not exist, create it and set its position
+                // If the agent does not exist, create it at the agent's position
                 agents[agent.id] = Instantiate(agentPrefab, new Vector3(0, 0, 0), Quaternion.identity);
                 agents[agent.id].GetComponent<CarTransforms>().SetMove(newAgentPosition);
             }
@@ -294,137 +300,135 @@ IEnumerator SendConfiguration()
         {
             if (!receivedIds.Contains(agentPair.Key))
             {
-                // If the id of the agent is not in the set of received ids, add it to the list of ids to delete
+                 // If the id of the agent is not in the set of received ids, add it to the list of ids to delete
                 idsToDelete.Add(agentPair.Key);
             }
         }
 
         foreach (var id in idsToDelete)
+    {
+        if (agents.TryGetValue(id, out GameObject agentToDelete))
         {
-            if (agents.TryGetValue(id, out GameObject agentToDelete))
+            // Get the CarTransforms component and call GetWheelObjects
+            CarTransforms carTransforms = agentToDelete.GetComponent<CarTransforms>();
+            List<GameObject> wheelsToDelete = carTransforms.GetWheelObjects();
+
+            foreach(GameObject wheel in wheelsToDelete)
             {
-                // Get the CarTransforms component and call GetWheelObjects
-                CarTransforms carTransforms = agentToDelete.GetComponent<CarTransforms>();
-                List<GameObject> wheelsToDelete = carTransforms.GetWheelObjects();
-
-                foreach(GameObject wheel in wheelsToDelete)
-                {
-                    // Destroy each wheel
-                    Destroy(wheel);
-                }
-
-                // Destroy the agent and remove it from the dictionary
-                Destroy(agentToDelete);
-                agents.Remove(id);
+                // Destroy each wheel
+                Destroy(wheel); 
             }
+
+            Destroy(agentToDelete); // Destroy the agent
+            agents.Remove(id); // Remove from the dictionary
         }
-    
-        // Set the updated flag to true and the started flag to true if it is not already
-        updated = true;
-        if(!started) started = true;
     }
-}
+             // Set the updated flag to true and the started flag to true if it is not already
+            updated = true;
+            if(!started) started = true;
+        }
+    }
 
     // This method gets the data of the obstacles
-IEnumerator GetObstacleData() 
-{
-    // Send a GET request to the server to get the data of the obstacles
-    UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
-    yield return www.SendWebRequest();
- 
-    if (www.result != UnityWebRequest.Result.Success)
-        // If the request failed, log the error
-        Debug.Log(www.error);
-    else 
+    IEnumerator GetObstacleData() 
     {
-        // If the request succeeded, parse the received data
-        obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-
-        foreach(AgentData obstacle in obstacleData.positions)
+        // Send a GET request to the server to get the data of the obstacles
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getObstaclesEndpoint);
+        yield return www.SendWebRequest();
+    
+        if (www.result != UnityWebRequest.Result.Success)
+            // If the request failed, log the error
+            Debug.Log(www.error);
+        else 
         {
-            // For each obstacle in the received data, create an obstacle object at the obstacle's position
-            GameObject obstacleObject = Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
-            // Get the Building component of the obstacle object and set its color
-            Building buildingComponent = obstacleObject.GetComponent<Building>();
-            buildingComponent.SetColor();
+            // If the request succeeded, parse the received data
+            obstacleData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+
+            foreach(AgentData obstacle in obstacleData.positions)
+            {
+                // For each obstacle in the received data, create an obstacle object at the obstacle's position
+                GameObject obstacleObject = Instantiate(obstaclePrefab, new Vector3(obstacle.x, obstacle.y, obstacle.z), Quaternion.identity);
+                // Get the Building component of the obstacle object and set its color
+                Building buildingComponent = obstacleObject.GetComponent<Building>();
+                buildingComponent.SetColor();
+            }
         }
     }
-}
 
-   IEnumerator GetTrafficLightsData() 
-{
-    // Send a GET request to the server to get the data of the traffic lights
-    UnityWebRequest www = UnityWebRequest.Get(serverUrl + getTrafficLightsEndpoint);
-    yield return www.SendWebRequest();
- 
-    if (www.result != UnityWebRequest.Result.Success)
-        // If the request failed, log the error
-        Debug.Log(www.error);
-    else 
+    IEnumerator GetTrafficLightsData() 
     {
-        // If the request succeeded, parse the received data
-        trafficLightsData = JsonUtility.FromJson<TrafficLightsData>(www.downloadHandler.text);
-
-        foreach(TrafficLigthData trafficLigth in trafficLightsData.positions)
+        // Send a GET request to the server to get the data of the traffic lights
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getTrafficLightsEndpoint);
+        yield return www.SendWebRequest();
+    
+        if (www.result != UnityWebRequest.Result.Success)
+            // If the request failed, log the error
+            Debug.Log(www.error);
+        else 
         {
-            if(trafficLights.ContainsKey(trafficLigth.id))
+            // If the request succeeded, parse the received data
+            trafficLightsData = JsonUtility.FromJson<TrafficLightsData>(www.downloadHandler.text);
+
+            foreach(TrafficLigthData trafficLigth in trafficLightsData.positions)
             {
-                // If the traffic light already exists, update its state
-                trafficLights[trafficLigth.id].GetComponent<TrafficLight>().SetState(trafficLigth.state);
-            }
-            else
-            {
+                if(trafficLights.ContainsKey(trafficLigth.id))
+                {
+                    // If the traffic light already exists, update its state
+                    trafficLights[trafficLigth.id].GetComponent<TrafficLight>().SetState(trafficLigth.state);
+                }
+                else
+                {
                 // If the traffic light does not exist, create it at the traffic light's position and set its direction
-                trafficLights[trafficLigth.id] = Instantiate(trafficLigthPrefab, new Vector3(trafficLigth.x, trafficLigth.y, trafficLigth.z), Quaternion.identity);
+                trafficLights[trafficLigth.id]=Instantiate(trafficLigthPrefab, new Vector3(trafficLigth.x, trafficLigth.y, trafficLigth.z), Quaternion.identity);
+                // Set the direction of the traffic light
                 trafficLights[trafficLigth.id].GetComponent<TrafficLight>().SetDirection(trafficLigth.direction);
-                // Also, instantiate a road at the traffic light's position
                 Instantiate(roadPrefab, new Vector3(trafficLigth.x, trafficLigth.y, trafficLigth.z), Quaternion.identity);
-            }
+                }
         }
     } 
 }
 
-  IEnumerator GetRoadsData() 
-{
-    // Send a GET request to the server to get the data of the roads
-    UnityWebRequest www = UnityWebRequest.Get(serverUrl + getRoadsEndpoint);
-    yield return www.SendWebRequest();
- 
-    if (www.result != UnityWebRequest.Result.Success)
-        // If the request failed, log the error
-        Debug.Log(www.error);
-    else 
-    {
-        // If the request succeeded, parse the received data
-        roadsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-
-        foreach(AgentData road in roadsData.positions)
+    IEnumerator GetRoadsData() 
         {
-            // For each road in the received data, create a road object at the road's position
-            Instantiate(roadPrefab, new Vector3(road.x, road.y, road.z), Quaternion.identity);
+            // Send a GET request to the server to get the data of the roads
+            UnityWebRequest www = UnityWebRequest.Get(serverUrl + getRoadsEndpoint);
+            yield return www.SendWebRequest();
+        
+            if (www.result != UnityWebRequest.Result.Success)
+                // If the request failed, log the error
+                Debug.Log(www.error);
+            else 
+            {
+                // If the request succeeded, parse the received data
+                roadsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+
+                foreach(AgentData road in roadsData.positions)
+                {
+                    // For each road in the received data, create a road object at the road's position
+                    Instantiate(roadPrefab, new Vector3(road.x, road.y, road.z), Quaternion.identity);
+                }
+            }
+        }
+
+    IEnumerator GetDestinationsData() 
+    {
+        // Send a GET request to the server to get the data of the destinations
+        UnityWebRequest www = UnityWebRequest.Get(serverUrl + getDestinationsEndpoint);
+        yield return www.SendWebRequest();
+    
+        if (www.result != UnityWebRequest.Result.Success)
+            // If the request failed, log the error
+            Debug.Log(www.error);
+        else 
+        {
+            // If the request succeeded, parse the received data
+            destinationsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
+
+            foreach(AgentData destination in destinationsData.positions)
+            {
+                // For each destination in the received data, create a destination object at the destination's position
+                Instantiate(destinationPrefab, new Vector3(destination.x, destination.y, destination.z), Quaternion.identity);
+            }
         }
     }
-}
-
-   IEnumerator GetDestinationsData() 
-{
-    // Send a GET request to the server to get the data of the destinations
-    UnityWebRequest www = UnityWebRequest.Get(serverUrl + getDestinationsEndpoint);
-    yield return www.SendWebRequest();
- 
-    if (www.result != UnityWebRequest.Result.Success)
-        // If the request failed, log the error
-        Debug.Log(www.error);
-    else 
-    {
-        // If the request succeeded, parse the received data
-        destinationsData = JsonUtility.FromJson<AgentsData>(www.downloadHandler.text);
-
-        foreach(AgentData destination in destinationsData.positions)
-        {
-            // For each destination in the received data, create a destination object at the destination's position
-            Instantiate(destinationPrefab, new Vector3(destination.x, destination.y, destination.z), Quaternion.identity);
-        }
-    }
-}
 }
